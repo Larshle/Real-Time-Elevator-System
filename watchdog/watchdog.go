586f2a1bec1b53c30bdcf6820696c6cc070d216d
Larshle <125282510+Larshle@users.noreply.github.com>
2024-03-12 22:08:26 +0100

    "i retning av noe fungerende watchdog"

--- watchdog/watchdog.go
+++ watchdog/watchdog.go
@@ -2,8 +2,32 @@ package watchdog
 
 import (
 	"time"
+	"fmt"
 )
 
-func Watchdog(){
-	
+func Watchdog(seconds int, barkC chan<- bool, startMovingC <-chan bool, stopMovingC <-chan bool) {
+	timer := time.NewTimer(time.Duration(seconds) * time.Second)
+	timer.Stop() 
+
+	for {
+		select {
+		case <-stopMovingC:
+			timer.Stop() 
+			//timer.Reset(time.Duration(seconds) * time.Second)
+			fmt.Println("Stopped Moving: Timer reset.")
+			barkC <- false
+			
+
+		case <-startMovingC:
+			timer = time.NewTimer(5*time.Second)
+			fmt.Println("Started Moving: Timer started.")
+			barkC <- false
+
+		case <-timer.C:
+			fmt.Println("IM STUCK!!!")
+			
+			barkC <- true
+			
+		}
+	}
 }
